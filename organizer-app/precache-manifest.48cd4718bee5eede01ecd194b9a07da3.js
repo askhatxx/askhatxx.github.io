e028cc71bdec1465b799c74b6a3febf5bc97d125
askhatxx <55058367+askhatxx@users.noreply.github.com>
2019-12-01 20:08:00 +0600

    Add organizer-app

---
+++ organizer-app/precache-manifest.48cd4718bee5eede01ecd194b9a07da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339049388e27b69ac1421868bf6ed62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83246a5083850c783f20",
+    "url": "/static/css/main.d371a565.chunk.css"
+  },
+  {
+    "revision": "f6421cdda13a33c93546",
+    "url": "/static/js/2.8e4a85bc.chunk.js"
+  },
+  {
+    "revision": "83246a5083850c783f20",
+    "url": "/static/js/main.3d67bcfe.chunk.js"
+  },
+  {
+    "revision": "68b2e9825d38c4a6f29b",
+    "url": "/static/js/runtime-main.a2f4f66e.js"
+  }
+]);
