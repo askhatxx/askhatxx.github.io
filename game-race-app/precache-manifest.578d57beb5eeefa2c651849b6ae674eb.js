6b45abe32c77130c1eccd06b5b0c73615d035919
askhatxx <55058367+askhatxx@users.noreply.github.com>
2019-12-26 23:11:04 +0600

    Add game-race-app

---
+++ game-race-app/precache-manifest.578d57beb5eeefa2c651849b6ae674eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3afdea1afb7432ab19f3850e022577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39a0c2f2222673d15b2e",
+    "url": "/static/css/main.48c158c0.chunk.css"
+  },
+  {
+    "revision": "7b68885c832fec5bc13a",
+    "url": "/static/js/2.f5e7b6d1.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.f5e7b6d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39a0c2f2222673d15b2e",
+    "url": "/static/js/main.23790386.chunk.js"
+  },
+  {
+    "revision": "37562257d11a9fd06f5b",
+    "url": "/static/js/runtime-main.25c8b36f.js"
+  }
+]);
