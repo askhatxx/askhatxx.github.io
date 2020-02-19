62e6763c0d8d1e4e38a30146439241b8099b9ad1
askhatxx <55058367+askhatxx@users.noreply.github.com>
2020-02-20 00:43:46 +0600

    Add human-qualities-shop

---
+++ human-qualities-shop/precache-manifest.0df9ed59867398f388812a289131efe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca99ab4d4febe39a64e6c8cbfc4b5be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce118bb391b189a5ee32",
+    "url": "/static/css/main.29e33882.chunk.css"
+  },
+  {
+    "revision": "3e07781328078f87dcea",
+    "url": "/static/js/2.4d1c1164.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4d1c1164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce118bb391b189a5ee32",
+    "url": "/static/js/main.403d741a.chunk.js"
+  },
+  {
+    "revision": "8e7ada8fb21668d064a3",
+    "url": "/static/js/runtime-main.d33ad5f2.js"
+  }
+]);
