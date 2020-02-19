7a9f3b9d05b1022df0309d9dee22c36b843ed6ae
askhatxx <55058367+askhatxx@users.noreply.github.com>
2020-02-20 02:00:21 +0600

    Commit

---
+++ human-qualities-shop/precache-manifest.482910db79c3edfa7f6a9b485d0b4341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030bc3e25f45c323c95ea70e78a6d143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11be4bd9d15acadd5e97",
+    "url": "/static/css/main.4eac51e1.chunk.css"
+  },
+  {
+    "revision": "170e46b91bc5c7e582b6",
+    "url": "/static/js/2.083eba85.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.083eba85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11be4bd9d15acadd5e97",
+    "url": "/static/js/main.9228a302.chunk.js"
+  },
+  {
+    "revision": "8e7ada8fb21668d064a3",
+    "url": "/static/js/runtime-main.d33ad5f2.js"
+  }
+]);
--- human-qualities-shop/service-worker.js
+++ human-qualities-shop/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02c2976fe1b575001c17841a22ec75f2.js"
+  "/precache-manifest.482910db79c3edfa7f6a9b485d0b4341.js"
 );
 
 self.addEventListener('message', (event) => {
