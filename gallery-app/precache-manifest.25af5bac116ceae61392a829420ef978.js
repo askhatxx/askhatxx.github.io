6b021dd69943d27e3f304d0ae5118d8d4564d6dd
askhatxx <55058367+askhatxx@users.noreply.github.com>
2020-02-27 23:15:21 +0600

    Add gallery-app

---
+++ gallery-app/precache-manifest.25af5bac116ceae61392a829420ef978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11776732179fc01b23e39b789f2fdce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb198eafe04c39e06c90",
+    "url": "/static/css/main.3b7bcc5d.chunk.css"
+  },
+  {
+    "revision": "7ab3bf0ffb67269d6778",
+    "url": "/static/js/2.1fc9d6a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1fc9d6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb198eafe04c39e06c90",
+    "url": "/static/js/main.56a9621f.chunk.js"
+  },
+  {
+    "revision": "f2dba5960a18e475c25f",
+    "url": "/static/js/runtime-main.1da12dd0.js"
+  }
+]);
